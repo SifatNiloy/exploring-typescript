de7f742a7dd9e685306e485dc940c2500b1f154e
SifatNiloy <sifatniloy18@gmail.com>
2024-05-01 10:13:03 +0600

    Adding TypeScript files

---
+++ module1/dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+var course = "next level web dev";
+console.log(course);
---
+++ module1/src/1.4.ts
@@ -0,0 +1,12 @@
+// basic data type
+
+//string
+let firstName: String ="Sifat";
+//number
+let roll= 29;
+//boolean
+let isAdmin= true
+// undefined
+let x: undefined = undefined;
+// null
+let y: null = null;
