36b2308dac15699a9bb1630b64f1ef996776a44d
SifatNiloy <sifatniloy18@gmail.com>
2025-06-15 14:01:20 +0600

    add examples of spread, rest, and destructuring

--- module1/src/1.7.ts
+++ module1/src/1.7.ts
@@ -1,63 +1,116 @@
-{ //scope block by curly braces
-    
-//spread operator 
-//rest operator
-//destructuring
+{ // ---------- Scope Block ----------
+  // This block is wrapped in curly braces `{}` to create a local scope.
+  // Variables declared inside this block won't affect or be accessible outside.
 
+  // ---------- SPREAD OPERATOR (`...`) ----------
+  // Used to "spread out" or expand elements from arrays or properties from objects
 
+  const bros1: string[] = ["sifat", "hossain", "niloy"];
+  const bros2: string[] = ["abdur", "rashid", "sawom"];
 
-// spread ...
-const bros1:string[]=["sifat", "hossain", "niloy"]
-const bros2:string[]=["abdur", "rashid", "sawom"]
+  // Goal: Merge `bros2` into `bros1` using the spread operator
+  // `...bros2` takes each element from bros2 and passes it individually to push
+  bros1.push(...bros2); 
+  // bros1 becomes: ["sifat", "hossain", "niloy", "abdur", "rashid", "sawom"]
 
-bros1.push(...bros2)
-const mentor1={
+  // --- Spread with Objects ---
+  // Combining two objects into one by "spreading" their properties
+
+  const mentor1 = {
     typescript: 'mezba',
     redux: 'mir',
     dbms: 'mizan',
-}
-const mentor2={
-    prisma: 'firoz', 
-    next:"tonmaoy", 
+  };
+
+  const mentor2 = {
+    prisma: 'firoz',
+    next: "tonmoy",  // Note: "tonmoy" was previously misspelled as "tonmaoy"
     cloud: 'nahid'
-}
-const mentorList={
-    ...mentor1,
-    ...mentor2
-}
+  };
 
-//rest operator 
-// const greetFriends=(friend1:string, friend2:string, friend3:string)=>{
-//     console.log(`hi ${friend1}  ${friend2} ${friend3}`)
-// }
-const greetFriends=(...friends:string[])=>{
-    // console.log(`hi ${friend1}  ${friend2} ${friend3}`)
-    friends.forEach((friend: string)=>console.log(`hi ${friend}`))
-}
+  // Merging mentor1 and mentor2 into a new object `mentorList`
+  const mentorList = {
+    ...mentor1,  // spreads all key-value pairs from mentor1
+    ...mentor2   // then spreads all key-value pairs from mentor2
+  };
+  // Result:
+  // mentorList = {
+  //   typescript: 'mezba',
+  //   redux: 'mir',
+  //   dbms: 'mizan',
+  //   prisma: 'firoz',
+  //   next: 'tonmoy',
+  //   cloud: 'nahid'
+  // }
 
-greetFriends('abul', 'babul', 'kabul', "labul", "ubul");
+  // If keys are the same in both objects, later ones override earlier ones
 
+  // ---------- REST OPERATOR (`...`) ----------
+  // Gathers multiple values into an array
+  // Opposite of spread: instead of expanding, it "packs" arguments
 
-//destructuring
+  // Example: greeting any number of friends
+  const greetFriends = (...friends: string[]) => {
+    // Here, `friends` becomes an array containing all arguments passed in
+    // Example: ['abul', 'babul', 'kabul', 'labul', 'ubul']
+    
+    // Loop through each friend and print a greeting
+    friends.forEach((friend: string) => {
+      console.log(`hi ${friend}`);
+    });
+  };
 
-//object destructuring
-const user={
+  // Function call with multiple friend names
+  greetFriends('abul', 'babul', 'kabul', 'labul', 'ubul');
+  // Output:
+  // hi abul
+  // hi babul
+  // hi kabul
+  // hi labul
+  // hi ubul
+
+  // The rest operator is useful for:
+  // - Accepting variable numbers of arguments
+  // - Handling inputs as a group
+
+  // ---------- DESTRUCTURING ----------
+  // Allows unpacking properties or elements directly into variables
+
+  // --- Object Destructuring ---
+  const user = {
     id: 345,
     name: {
-        firstName: "sifat",
-        middleName: "hossain",
-        lastName: "niloy",
+      firstName: "sifat",
+      middleName: "hossain",
+      lastName: "niloy",
     },
     contactNo: 30289449,
     address: 'Dhaka',
-};
+  };
 
-// nicher line ta te type declare korte parbo na , name alias hoye jabe. jemon middleName: midname likhle midname hisabei pabe
-const {contactNo, name:{middleName}}= user;
+  // Extracting nested values from object
+  // Syntax: { outerProperty, nestedProperty: { innerProperty } }
 
+  const {
+    contactNo,  // gets value from user.contactNo
+    name: { middleName } // gets user.name.middleName
+  } = user;
 
-//arrray destructuring
-const myFriends=['siam', 'shovon', 'wazed', 'sawom', 'fithfriend', 'sixthfriend']
-const [,,thirdFriend,...rest]= myFriends;  //rest will get the fifth and sixth friend
+  // Note:
+  // You can't declare `name` directly here because you're going inside it to get `middleName`
+  // Also, you can alias: `middleName: midName` if you want to rename the variable
+
+  // --- Array Destructuring ---
+  const myFriends = ['siam', 'shovon', 'wazed', 'sawom', 'fifthfriend', 'sixthfriend'];
+
+  // Use commas to skip elements, and use rest to collect remaining ones
+  const [, , thirdFriend, ...rest] = myFriends;
+  // Skips first two: 'siam', 'shovon'
+  // thirdFriend = 'wazed'
+  // rest = ['sawom', 'fifthfriend', 'sixthfriend']
+
+  // Destructuring is powerful for:
+  // - Cleanly extracting values from arrays/objects
+  // - Making code more readable and concise
 
 }
